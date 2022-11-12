88fc204d3bd5e9eabfd1814f08e92498a276b6a4
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-11-12 07:53:00 +0530

    class base

---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Post extends Component {
+    render(){
+        return(
+            <div>
+                <h1>
+                    hello 2
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Post;
