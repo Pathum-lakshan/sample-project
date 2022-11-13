2b1cc033fc7c316252b284922d6d48b2fa4cb7e6
Pathum Lakshan <54094629+Pathum-lakshan@users.noreply.github.com>
2022-11-13 07:31:11 +0530

    up

---
+++ src/Component1.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Component1 extends Component {
+    render(){
+        return(
+            <div>
+            <h1>
+                class base
+            </h1>
+        </div>
+        );
+    }
+}
+
+export default Component1;
